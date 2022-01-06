10bdb0bb3923e6825de2df36d82d0e4304ff5b75
Hernán David Figueroa <davshn@gmail.com>
2022-01-06 16:19:09 -0500

    login and register done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,31 @@
 import { useState } from 'react';
 import ButtonGen from '../generiComponents/ButtonGen';
 import { SectionStyled,TextStyled,ViewStyled,InputStyled,FormStyled,FormError,TitleStyled } from '../generiComponents/GenericStyles';
+import axios from 'axios';
+
 
 export default function Login() {
   const initialState = { //Estado inicial para usuarios
-    mail: "",
+    email: "",
     password: "",
   };
+  
+  const [token, setToken] = useState("");
   const [input, setInput] = useState(initialState); //Crea el estado que contiene los datos
   const [errors,setErrors] = useState({});  //Crea el estado que contendrá los errores
-   
+  
+  function loginUser(input){
+        axios.post('https://find-spot.herokuapp.com/login',input) //Envia por post la a crear
+          .then((res) => {
+            console.log(res);
+          })
+        .catch((res)=>console.log(res));  
+  }
+  
   function validate(input) {
     let error={};       //Guarda temporalmente los errores encontrados
-    if(!input.mail){error.mail="Requerido"}
-    else if(!(/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/i).test(input.mail)){error.mail="Correo invalido"};
+    if(!input.email){error.email="Requerido"}
+    else if(!(/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/i).test(input.email)){error.email="Correo invalido"};
     if (!input.password) { error.password = "Requerido" };
     return error
   };
@@ -26,15 +38,15 @@ export default function Login() {
     <ViewStyled>
       <TitleStyled>FindSpot</TitleStyled>
       <FormStyled>
-        <InputStyled value={input.mail} onChangeText={(ev)=>hadleInputChange("mail",ev)} placeholder="Correo" placeholderTextColor='gray' keyboardType='email-address'/>
-        {errors.mail&&(<FormError>{errors.mail}</FormError>)}
+        <InputStyled value={input.email} onChangeText={(ev)=>hadleInputChange("email",ev)} placeholder="Correo" placeholderTextColor='gray' keyboardType='email-address'/>
+        {errors.email&&(<FormError>{errors.email}</FormError>)}
       </FormStyled>
       <FormStyled>
         <InputStyled value={input.password} onChangeText={(ev)=>hadleInputChange("password",ev)} placeholder="Contraseña" placeholderTextColor='gray' secureTextEntry/>
         {errors.password&&(<FormError>{errors.password}</FormError>)}
       </FormStyled>
       <SectionStyled>
-        <ButtonGen title="Acceder" onPress={()=>setErrors(validate(input))}/>
+        <ButtonGen title="Acceder" onPress={()=>loginUser(input)}/>
         <TextStyled style={{ color: "red" }} >Olvidaste tu contraseña</TextStyled>
       </SectionStyled>
       <SectionStyled>
