cf3a06ff94f669f35674a1c0955fcb23c7eae018
Hernán David Figueroa <davshn@gmail.com>
2022-01-07 15:11:12 -0500

    fixed merge

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,7 +3,7 @@ import { backgroundColor } from "../services/theme.js";
 import styled from "styled-components/native";
 import SearchInput from '../components/Searchbar';
 import { EventCards } from "./EventCards.js";
-import ButtonGen from "./ButtonGen.js";
+import ButtonGen from '../generiComponents/ButtonGen';
 
 // const StyledView = styled.ScrollView`
 //   background-color: ${backgroundColor};
