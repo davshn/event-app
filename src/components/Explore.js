20ac89b83df27d62d20fc385cfb4dd25ac90854a
luis miranda <luismiguel1115@gmail.com>
2022-01-11 22:44:09 -0500

    test

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,6 +1,6 @@
 import { StyledTitle, StyledView } from "../generiComponents/GenericStyles";
 import Searchbar from "./Searchbar";
-import { MapView, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 import styled from "styled-components/native";
 
@@ -17,11 +17,11 @@ function Explore() {
         <StyledMap showsUserLocation loadingEnabled
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-       }}
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
        >
         </StyledMap>
       </StyledView>
