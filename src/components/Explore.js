ffc6da0604f5b5a7f184f2ecdc3a0ea977fa6508
luis miranda <luismiguel1115@gmail.com>
2022-01-12 10:05:59 -0500

    test

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,6 +1,6 @@
 import { StyledTitle, StyledView } from "../generiComponents/GenericStyles";
 import Searchbar from "./Searchbar";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout, Circle } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 import styled from "styled-components/native";
 
@@ -19,10 +19,25 @@ function Explore() {
           region={{
             latitude: 37.78825,
             longitude: -122.4324,
-            latitudeDelta: 0.015,
+            latitudeDelta: 0.09,
             longitudeDelta: 0.0121,
           }}
-       >
+        >
+          <Marker
+            // Reemplazar coordinate con variable auto de location para el usuario
+            coordinate={{latitude: 37.7825259, longitude: -122.4351431}}
+            title={"Ubicación actual"}
+          >
+            <Callout>
+              <Text>Detalles del evento y link para pagina detalle</Text>
+              <image source={require('../../assets/icon1.png')} />
+            </Callout>
+          </Marker>
+          <Circle
+            center={{latitude: 37.7825259, longitude: -122.4351431}}
+            radius={1000}
+            fillColor={'rgba(200, 300, 200, 05)'}
+          />
         </StyledMap>
       </StyledView>
     </>
--- src/components/ParaTi.js
+++ src/components/ParaTi.js
@@ -1,21 +1,57 @@
-import { Text } from "react-native"
-import {backgroundColor} from "../services/theme.js"
-import styled from "styled-components/native"
+// import { Text } from "react-native"
+// import {backgroundColor} from "../services/theme.js"
+// import styled from "styled-components/native"
 
-const StyledView = styled.ScrollView`
-    background-color: ${backgroundColor};
-`
+// const StyledView = styled.ScrollView`
+//     background-color: ${backgroundColor};
+// `
 
-function ParaTiScreen(){
-    return(
-        <StyledView>
-            <Text>
-                PARA TI
-            </Text>
-        </StyledView>
-    )
+// function ParaTiScreen(){
+//     return(
+//         <StyledView>
+//             <Text>
+//                 PARA TI
+//             </Text>
+//         </StyledView>
+//     )
+// }
+
+
+
+// export default ParaTiScreen
+
+
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function ParaTiScreen() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
 }
-
-
-
-export default ParaTiScreen
