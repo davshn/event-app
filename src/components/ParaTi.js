16370afeac13280eee3dacf7b7483874924732ec
Hernán David Figueroa <davshn@gmail.com>
2022-01-12 11:00:57 -0500

    user location done, need to be centered

--- src/components/ParaTi.js
+++ src/components/ParaTi.js
@@ -1,57 +1,21 @@
-// import { Text } from "react-native"
-// import {backgroundColor} from "../services/theme.js"
-// import styled from "styled-components/native"
+import { Text } from "react-native"
+import {backgroundColor} from "../services/theme.js"
+import styled from "styled-components/native"
 
-// const StyledView = styled.ScrollView`
-//     background-color: ${backgroundColor};
-// `
+const StyledView = styled.ScrollView`
+     background-color: ${backgroundColor};
+ `
 
-// function ParaTiScreen(){
-//     return(
-//         <StyledView>
-//             <Text>
-//                 PARA TI
-//             </Text>
-//         </StyledView>
-//     )
-// }
-
-
-
-// export default ParaTiScreen
+ function ParaTiScreen(){
+     return(
+         <StyledView>
+             <Text>
+                 PARA TI
+             </Text>
+        </StyledView>
+     )
+ }
 
 
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-import * as Location from 'expo-location';
 
-export default function ParaTiScreen() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  return (
-    <View >
-      <Text >{text}</Text>
-    </View>
-  );
-}
+export default ParaTiScreen
