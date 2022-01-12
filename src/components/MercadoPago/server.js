330ee18937ab96005ce525868df3b4bf12f25eb6
outsiderintrospector <juansebcarv@outlook.com>
2022-01-11 23:29:01 -0300

    implementation MP 1step

---
+++ src/components/MercadoPago/server.js
@@ -0,0 +1,7 @@
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+  access_token:
+    "TEST-7039161888137857-011115-7acfdca809308e76679c5691712bc69b-203515476",
+});
