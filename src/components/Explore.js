7e19fe0c4acd89865df5304fb620703473029b95
luis miranda <luismiguel1115@gmail.com>
2022-01-11 22:38:39 -0500

    styled map test

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -14,7 +14,16 @@ function Explore() {
     <>
       <Searchbar />
       <StyledView>
-        <StyledMap showsUserLocation loadingEnabled />
+        <StyledMap showsUserLocation loadingEnabled
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+       }}
+       >
+        </StyledMap>
       </StyledView>
     </>
   );
