c1f98f6dd99a6fc84689db9f6241c26504795a22
Camila <camilaluciasosa@hotmail.com>
2022-01-18 16:20:13 -0300

    Terms & Conditions

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,15 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import ButtonGen from '../generiComponents/ButtonGen';
-import { InputStyled, FormError,SmallerText, StyledTitle, SelectedDate, StyledView, ViewBackground, AgregarFotoButton, TextButton, ProfilePic } from '../generiComponents/GenericStyles';
+import { InputStyled, FormError,SmallerText, StyledTitle, SelectedDate, StyledView, ViewBackground, AgregarFotoButton, TextButton, ProfilePic, TextStyled, TermsText, TermsModal } from '../generiComponents/GenericStyles';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as ImagePicker from 'expo-image-picker';
 import {Alert} from 'react-native' ; 
 import axios from 'axios';
-import { Modal } from 'react-native';
+import { Modal, Text } from 'react-native';
 import { ModalContStyled, ModalText, ModalButtonStyled, ButtonText } from '../generiComponents/ModalGen';
 import CustomMultiPicker from "react-native-multiple-select-list";
 import { useSelector } from "react-redux"
+import { TermsConditions } from "./Terms&Contditions"
 
 export default function Register({ navigation }) {
   let selected = [];
@@ -27,7 +28,8 @@ export default function Register({ navigation }) {
   const [modalVisible, setModalVisible] = useState(false); //Controla el modal de error al crear usuario
   const modes = useSelector(state => state.darkModeReducer.darkMode);
   const categories = useSelector((state) => state.getCategoriesReducer.categories);
-  
+  const [termsmodalVisible, setTermsModalVisible] = useState(false);
+
   function createUser(user) {
     user.interests = selected;
     axios.post('https://find-spot.herokuapp.com/register',user) //Envia por post la a crear
@@ -131,7 +133,7 @@ export default function Register({ navigation }) {
         <SelectedDate style={{width: "90%", alignSelf: "center"}} onPress={showDatepicker}>Año de nacimiento: {input.dateOfBirth}</SelectedDate>
         {errors.dateOfBirth&&(<FormError>{errors.dateOfBirth}</FormError>)}
       <AgregarFotoButton onPress={pickImage}>
-        <TextButton>Agregar foto de perfil</TextButton>
+        <TextButton color={'#EDEDED'}>Agregar foto de perfil</TextButton>
       </AgregarFotoButton>
       {input.image && <ProfilePic source={{ uri: input.image }}/>}
       <SmallerText>Categorías:</SmallerText>
@@ -160,7 +162,16 @@ export default function Register({ navigation }) {
         selected={[]}
         border={"#776BC7"}
       />
+      <TextStyled style={{ color: "#999999"}}>Al registrarte, estarás aceptando nuestros <TermsText onPress={() => setTermsModalVisible(true)}>términos y condiciones</TermsText></TextStyled>
       <ButtonGen textcolor={'#EDEDED'} title="Enviar" onPress={() => validate(input)} />
+      <Modal animationType="fade" transparent={true} visible={termsmodalVisible}>
+        <TermsModal>
+          <TermsConditions/>
+          <ModalButtonStyled onPress={() => setTermsModalVisible(false)}>
+            <ButtonText>Aceptar</ButtonText>
+          </ModalButtonStyled>
+        </TermsModal>
+      </Modal>
       <Modal animationType="fade" transparent={true} visible={modalVisible}>
         <ModalContStyled>
           <ModalText>Verifica tus datos</ModalText>
