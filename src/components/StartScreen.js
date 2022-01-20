16d13c42d1bc1c2f25a4a61416aa9bbd7f981c24
Hernán David Figueroa <davshn@gmail.com>
2022-01-20 15:00:45 -0500

    moved initial search

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,12 @@
 import SearchInput from "./Searchbar";
 import { EventCards } from "./EventCards.js";
+import { searchByFilters } from '../stateManagement/actions/getEventsActions';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export default function StartScreen() {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(searchByFilters()), []);
   return (
     <>
       <SearchInput />
