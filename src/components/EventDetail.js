d32b21d0ecdb454939444a165d701f04fbb1fe81
Camila <camilaluciasosa@hotmail.com>
2022-01-20 10:58:31 -0300

    commit

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -12,6 +12,8 @@ import {
   TextButton,
   ViewBackground,
 } from "../generiComponents/GenericStyles";
+import { backgroundColor, TextColor } from "../services/theme";
+import { Text, View, ScrollView, Button } from "react-native"
 
 export default function EventDetail({ navigation: { goBack }, route }) {
   const [event, setEvent] = useState([]);
@@ -35,17 +37,61 @@ export default function EventDetail({ navigation: { goBack }, route }) {
   // cambiar el <TextCardMedium> por uno personalizado aca abajo para darle estilo y ver si aparecen por separado
   const tags = event.categories?.map(el => <TextCardMedium key={el.eventcategory.categoryId}>{el.name}</TextCardMedium> )
 
-  return (
-    <ViewBackground style={{ paddingBottom: 30 }}>
-      <DetailView style={{ height: "95%" }}>
-        <ImageDetail
-          source={{
-            uri:
-              event.eventPic ||
-              "https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg",
-          }}
-        />
-        <TextCardBig>{event.name}</TextCardBig>
+//   return (
+//     <ViewBackground style={{ paddingBottom: 30 }}>
+//       <DetailView style={{ height: "95%" }}>
+//         <ImageDetail
+//           source={{
+//             uri:
+//               event.eventPic ||
+//               "https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg",
+//           }}
+//         />
+//         <TextCardBig>{event.name}</TextCardBig>
+//         <DetailInfo>
+//           <TextCardMedium>{tags}</TextCardMedium>
+//           <TextCardSmall>Descripción : {event.description}</TextCardSmall>
+//           <TextCardMedium>Lugar: {event.place}</TextCardMedium>
+//           <TextCardSmall>Hora: {event.time}</TextCardSmall>
+//           <TextCardSmall>Fecha: {event.date}</TextCardSmall>
+//           <TextCardMedium>$ {event.price}</TextCardMedium>
+//           <TextCardMedium>Creado por : {event.user?.name}</TextCardMedium>
+//         </DetailInfo>
+
+//         <Payment
+//           precio={event.price}
+        
+//           evento={event.name}
+//           cantidad={5}
+//           fecha={event.date}
+//           hora={event.time}
+//         />
+
+//         <StyledButton onPress={() => goBack()}>
+//           <TextButton>Volver</TextButton>
+//         </StyledButton>
+//       </DetailView>
+//     </ViewBackground>
+//   );
+// }
+return (
+  <StyledView>
+    <ContainerImg style={{backgroundColor: "#5641abff"}} >
+    <View style={{position: "absolute", backgroundColor: "orange", height: "10%", width: "10%"}} onPress={() => goBack()}>
+      </View>
+      <BgImage
+         source={{
+          uri:
+            event.eventPic ||
+            "https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg",
+        }}
+      />
+      
+
+      <BottomContainer>
+        <TextName>{event.name}</TextName>
+        <Text style={{ color: "grey", bottom: "7%" }}>{event.place}</Text>
+        <View>
         <DetailInfo>
           <TextCardMedium>{tags}</TextCardMedium>
           <TextCardSmall>Descripción : {event.description}</TextCardSmall>
@@ -55,24 +101,58 @@ export default function EventDetail({ navigation: { goBack }, route }) {
           <TextCardMedium>$ {event.price}</TextCardMedium>
           <TextCardMedium>Creado por : {event.user?.name}</TextCardMedium>
         </DetailInfo>
-
-        <Payment
-          precio={event.price}
+        </View>
         
-          evento={event.name}
-          cantidad={5}
-          fecha={event.date}
-          hora={event.time}
-        />
-
-        <StyledButton onPress={() => goBack()}>
-          <TextButton>Volver</TextButton>
-        </StyledButton>
-      </DetailView>
-    </ViewBackground>
-  );
+          <Payment
+            precio={event.price}
+          
+            evento={event.name}
+            cantidad={5}
+            fecha={event.date}
+            hora={event.time}
+          />
+          <Button title="volver" onPress={() => goBack()}/>
+           <StyledButton >
+            <TextButton>Volver</TextButton>        
+          </StyledButton>
+         
+      </BottomContainer>
+    </ContainerImg>
+  </StyledView>
+);
 }
 
+const ContainerImg = styled.View`
+flex: 1;
+justify-content: center;
+align-items: center;
+`;
+const BgImage = styled.Image`
+position: absolute;
+width: 100%;
+height: 100%;
+justify-content: center;
+`;
+
+const BottomContainer = styled.View`
+margin-top: 72%;
+height: 90%;
+width: 400px;
+background-color: ${backgroundColor};
+border-radius: 70px;
+align-items: center;
+`;
+
+const StyledView = styled.ScrollView`
+  background-color: ${backgroundColor};
+`;
+const TextName = styled.Text`
+margin-top: 30%
+font-weight: bold;
+font-size: 23px;
+bottom: 8%;
+color: ${TextColor};
+`;
 const ImageDetail = styled.Image`
   width: 100%;
   height: 200px;
