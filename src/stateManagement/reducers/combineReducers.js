26d6f21f6013bdd717cd41eed30e936366992351
luis miranda <luismiguel1115@gmail.com>
2022-01-26 11:19:05 -0500

    persistant and erase functions

--- src/stateManagement/reducers/combineReducers.js
+++ src/stateManagement/reducers/combineReducers.js
@@ -4,9 +4,21 @@ import authUserReducer from './authUserReducer';
 import getEventsReducer from './getEventsReducer';
 import getCategoriesReducer from './getCategoriesReducer';
 import shopReducer from './shopReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
 //Recibe los reducers a combinar
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    // whitelist: ['shopReducer']
+  };
+
 export default combineReducers ({       
-    darkModeReducer,authUserReducer,getEventsReducer,getCategoriesReducer,shopReducer,
+    darkModeReducer,
+    authUserReducer,
+    getEventsReducer,
+    getCategoriesReducer,
+    shopReducer: persistReducer(persistConfig,shopReducer),
 })
